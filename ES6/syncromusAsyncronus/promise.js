f26f37857be22ab46dd56e365fae7ad7f05b3e55
Rayhan1313 <rayhanhimel1997@gmail.com>
2026-08-06 15:17:07 +0600

    promise part-1

--- ES6/syncromusAsyncronus/promise.js
+++ ES6/syncromusAsyncronus/promise.js
@@ -1,11 +1,49 @@
+console.log("Welcome");
+/*
 const promise1 = new Promise((resolve, reject) => {
-  let promise = false;
-  if (promise) {
-    resolve("you're Right on promise");
+  let promises = true;
+  if (promises) {
+    resolve("Hey welcome. You're in right place");
   } else {
-    reject("You're hopper");
+    reject(new Error("Hoooah! You're just a hopper"));
   }
 });
 
-promise1.then("End").catch("error");
-console.log(promise1);
+const promise2 = new Promise((resolve, reject) => {
+  resolve("Congratulations");
+});
+*/
+
+// race case
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Congrates brother. You're in right track");
+  }, 3000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("hey Man, What's Up?");
+  }, 3500);
+});
+
+//single promise run process
+/*
+
+promise1
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+promise2.then((res) => {
+  console.log(res);
+});
+*/
+// Promise.all([promise1, promise2]).then(([res1, res2]) => {
+Promise.race([promise1, promise2]).then((res) => {
+  console.log(res);
+});
+console.log("end");
